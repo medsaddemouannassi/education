ff1675db6eeda545abbc3c724b18b379ebb2b855
Mohamed Saddem Ouannassi <medsaddemouannassi@gmail.com>
2022-02-25 22:36:23 +0100

    deployment heroku

--- server.js
+++ server.js
@@ -1,2 +1,7 @@
 const app = require("./backend/app");
 app.listen(process.env.PORT || 3000, () => {});
+
+app.use(express.static(__dirname + "./dist/education"));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "./dist/education/index.html"));
+});
